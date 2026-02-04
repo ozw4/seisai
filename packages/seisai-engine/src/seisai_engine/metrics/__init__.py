975a84d79d06af0066107455fcec4b91753ee964
Mitsuyuki Ozawa <zerowarurei@gmail.com>
2026-02-04 11:04:23 +0000

    Add metrics module with phase pick metrics functions and exports

---
+++ packages/seisai-engine/src/seisai_engine/metrics/__init__.py
@@ -0,0 +1,14 @@
+from .phase_pick_metrics import (
+	compute_ps_metrics_from_batch,
+	masked_abs_error_1d,
+	pick_argmax_w,
+	summarize_abs_error,
+)
+
+__all__ = [
+	'compute_ps_metrics_from_batch',
+	'masked_abs_error_1d',
+	'pick_argmax_w',
+	'summarize_abs_error',
+]
+
