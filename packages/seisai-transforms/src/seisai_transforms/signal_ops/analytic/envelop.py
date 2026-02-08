6d0fe4ffcda00e44be06d9d2589b498cfc4c8537
Mitsuyuki Ozawa <zerowarurei@gmail.com>
2026-02-08 13:57:36 +0000

    Refactor: move compute_envelope function to a dedicated module and remove obsolete signal_ops file

---
+++ packages/seisai-transforms/src/seisai_transforms/signal_ops/analytic/envelop.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy.signal import hilbert
+
+
+def compute_envelope(x: np.ndarray, axis: int = -1) -> np.ndarray:
+	z = hilbert(x, axis=axis)  # complex analytic signal
+	env = np.abs(np.asarray(z))  # envelope = magnitude
+	return env.astype(x.dtype, copy=False)
