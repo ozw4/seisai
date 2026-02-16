d8ad937481dee8eaa7eda75740eee4ebec6e6adc
Mitsuyuki Ozawa <zerowarurei@gmail.com>
2026-02-16 23:30:08 +0000

    feat: update training scripts and configurations for new loss functions and parameters

--- proc/mae/run_prestage_sweep.sh
+++ proc/mae/run_prestage_sweep.sh
@@ -1,2 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_masked.yaml
-CUDA_VISIBLE_DEVICES=0 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_huber_masked_d1.yaml
+CUDA_VISIBLE_DEVICES=0 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_mse_all_w005.yaml
+CUDA_VISIBLE_DEVICES=0 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_mse_all_w01.yaml
+CUDA_VISIBLE_DEVICES=0 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_mse_all_w02.yaml
--- proc/mae/run_prestage_sweep2.sh
+++ proc/mae/run_prestage_sweep2.sh
@@ -1,2 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_shift4_masked.yaml
-CUDA_VISIBLE_DEVICES=1 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_shift8_masked.yaml
+
+CUDA_VISIBLE_DEVICES=1 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_fxmag_all_w005.yaml
+CUDA_VISIBLE_DEVICES=1 python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_fxmag_all_w01.yaml
+CUDA_VISIBLE_DEVICES=1  python examples/examples_train_blindtrace.py --config proc/mae/configs/loss_v2_l1_plus_fxmag_all_w02.yaml
