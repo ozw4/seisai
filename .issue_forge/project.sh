d7da7a6a4b2d18045eb279ff137c06249837ed8a
ozw4 <zerowarurei@gmail.com>
2026-05-17 16:46:41 +0000

    chore: address batch checks for issues #168-#170

--- .issue_forge/project.sh
+++ .issue_forge/project.sh
@@ -1,9 +1,12 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+
 # issue_forge consumer config.
 # Defaults are supplied by vendor/issue_forge.
 CODEX_FLOW_BASE_BRANCH='feature/fbpick2'
 CODEX_FLOW_BASE_REF='origin/feature/fbpick2'
 CODEX_FLOW_PROFILE_WRITE_SANDBOX='danger-full-access'
-CODEX_FLOW_PROFILE_WRITE_REASONING='high'
+CODEX_FLOW_PROFILE_WRITE_REASONING='xhigh'
 CODEX_FLOW_PROFILE_READ_SANDBOX='danger-full-access'
 CODEX_FLOW_PROFILE_READ_REASONING='medium'
 
